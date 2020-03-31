cc992dfb1943ba17a33a0c45448259c5ae5fd390
Angelo Gonçalves da Luz <angelogl@gmail.com>
2020-03-31 02:11:42 -0300

    :alembic: Adiciona exemplo de desestruturação
    Exemplo aplicado em objeto e em array de inteiros

---
+++ desestruturacao-utube/app.js
@@ -0,0 +1,43 @@
+//DADOS
+const valores = [4, 1, 2, 3, 4, 5]
+
+const cliente = {
+    nome: 'Angelo Luz',
+    cpf: '014.035.510.11',
+    nascimento: '23/06/1986',
+    telefone: '981560346',
+    endereco: {
+        rua: 'Avenida Javascript',
+        cidade: 'Pelotas',
+        estado: 'RS',
+        pais: 'Brasil',
+    },
+};
+
+//DESESTRUTURAÇÃO
+//TODO: Função que recebe um cliente e retorna apenas CPF, telefone e a cidade
+const {cpf, telefone, endereco:{cidade}} = cliente;
+console.log(cpf);
+console.log(telefone);
+console.log(cidade);
+
+function y (cliente){
+    console.log(cliente.cpf);
+    console.log(cliente.telefone);
+}
+
+function x ({cpf, telefone, endereco:{cidade}}){
+    console.log(cpf);
+    console.log('telefone :', telefone);
+    console.log('cidade: ', 'cidade');
+}
+
+x(cliente)
+
+//TODO: Pega o primeiro (e o terceiro) elemento do array de números (ignora o segundo)
+const [n1] = valores;
+console.log('n1 :', n1);
+
+const [v1, , v3] = valores;
+console.log('n1 :', v1);
+console.log('n3 :', v3);
