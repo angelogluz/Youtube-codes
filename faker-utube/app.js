944aa8a822baf7a9938ab3c77f5f2de32caef21b
lazaroap <48218476+lazaroap@users.noreply.github.com>
2020-04-06 21:17:33 -0300

    Adiciona comentario no app.js do faker-utube

--- faker-utube/app.js
+++ faker-utube/app.js
@@ -3,6 +3,8 @@ const faker = require('faker');
 faker.locale = 'pt_BR';
 const clients = [];
 
+
+//Cria n clientes com os atributos nome, email, telefone e data de nascimento
 for (let index = 0; index < 10; index++) {
     const name = faker.name.findName();
     const email = faker.internet.email(name);
