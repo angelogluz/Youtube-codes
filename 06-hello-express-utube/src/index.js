8df2f1318ef9686eb1a11670021895cc1ec04d6a
Sandro Sória Canez <90696452+sandro-soria-canez@users.noreply.github.com>
2022-04-04 08:46:06 -0300

    incremento de um item no array

--- 06-hello-express-utube/src/index.js
+++ 06-hello-express-utube/src/index.js
@@ -3,7 +3,7 @@ const express = require ('express');
 const app = express();
 
 app.get('/', (request, response) => {
-    response.json([1, 2, 3, 4]);
+    response.json([1, 2, 3, 4, 5]); /* incremento e um item no array*/
 })
 
 app.listen(3000);
